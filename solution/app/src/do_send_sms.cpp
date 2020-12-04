2ded27c31e9b637d1c66b65b70109e6fd082e8df
Wojciech Migda <wojtek.golf@interia.pl>
2020-12-04 12:04:11 +0100

    Extract execute()

--- solution/app/src/do_send_sms.cpp
+++ solution/app/src/do_send_sms.cpp
@@ -7,6 +7,7 @@
 #include "read.hpp"
 #include "serial_port_setup.hpp"
 #include "logging.hpp"
+#include "execute.hpp"
 
 #include "spdlog/spdlog.h"
 #include "fmt/core.h"
@@ -15,70 +16,6 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/system/error_code.hpp>
 #include <boost/asio/streambuf.hpp>
-
-
-/*
- * Utility to synchronously execute specified command
- */
-template<
-    bool isSet, bool isXTest, bool isXRead, bool isXWrite, bool isXExec,
-    typename Body
->
-int execute(
-    std::string const & device,
-    boost::asio::serial_port & sp,
-    Ringbeller::request<isSet, isXTest, isXRead, isXWrite, isXExec, Body> const & command)
-{
-    boost::system::error_code ec;
-
-    // Write to the modem
-
-    auto const wb = Ringbeller::write(sp, command, ec);
-
-    if (ec)
-    {
-        spdlog::error("Failed to write to the device {}, Reason: {}", device, ec.message());
-        return -1;
-    }
-
-    fmt::print("Sent {} bytes to the modem\n", wb);
-
-    // Read from the modem
-
-    boost::asio::streambuf buf;
-    Ringbeller::response<Ringbeller::string_body, Ringbeller::vector_sequence> response;
-
-    auto rb = Ringbeller::read(sp, buf, response, ec);
-
-    if (ec)
-    {
-        spdlog::error("Failed to read from the device {}, Reason: {}", device, ec.message());
-        return -1;
-    }
-
-    // Present the response
-    fmt::print("Read {} bytes from the modem\n", rb);
-
-    fmt::print("Response result code: {}\n", response.result_code);
-    if (not response.rc_text.empty())
-    {
-        fmt::print("Response result text: {}\n", response.rc_text);
-    }
-    else
-    {
-        fmt::print("Response result text is empty\n");
-    }
-    if (response.body.empty())
-    {
-        fmt::print("Response body is empty\n");
-    }
-    else for (auto const & line : response.body)
-    {
-        fmt::print("Response body: {}\n", line);
-    }
-
-    return 0;
-}
 
 
 int run_send_sms(
---
+++ solution/app/src/execute.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#ifndef APP_SRC_EXECUTE_HPP_
+#define APP_SRC_EXECUTE_HPP_
+
+#include "message.hpp"
+#include "write.hpp"
+#include "read.hpp"
+
+#include "spdlog/spdlog.h"
+#include "fmt/core.h"
+
+#include <boost/asio/serial_port.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/asio/streambuf.hpp>
+
+#include <string>
+
+/*
+ * Utility to synchronously execute specified command
+ */
+template<
+    bool isSet, bool isXTest, bool isXRead, bool isXWrite, bool isXExec,
+    typename Body
+>
+int execute(
+    std::string const & device,
+    boost::asio::serial_port & sp,
+    Ringbeller::request<isSet, isXTest, isXRead, isXWrite, isXExec, Body> const & command)
+{
+    boost::system::error_code ec;
+
+    // Write to the modem
+
+    auto const wb = Ringbeller::write(sp, command, ec);
+
+    if (ec)
+    {
+        spdlog::error("Failed to write to the device {}, Reason: {}", device, ec.message());
+        return -1;
+    }
+
+    fmt::print("Sent {} bytes to the modem\n", wb);
+
+    // Read from the modem
+
+    boost::asio::streambuf buf;
+    Ringbeller::response<Ringbeller::string_body, Ringbeller::vector_sequence> response;
+
+    auto rb = Ringbeller::read(sp, buf, response, ec);
+
+    if (ec)
+    {
+        spdlog::error("Failed to read from the device {}, Reason: {}", device, ec.message());
+        return -1;
+    }
+
+    // Present the response
+    fmt::print("Read {} bytes from the modem\n", rb);
+
+    fmt::print("Response result code: {}\n", response.result_code);
+    if (not response.rc_text.empty())
+    {
+        fmt::print("Response result text: {}\n", response.rc_text);
+    }
+    else
+    {
+        fmt::print("Response result text is empty\n");
+    }
+    if (response.body.empty())
+    {
+        fmt::print("Response body is empty\n");
+    }
+    else for (auto const & line : response.body)
+    {
+        fmt::print("Response body: {}\n", line);
+    }
+
+    return 0;
+}
+
+
+#endif /* APP_SRC_EXECUTE_HPP_ */
